a38c464e7fcccd4c29bdaa30d6778d5d5e9d106a
Nicolas Evangelista <nicolas.evangelista@globallogic.com>
2023-11-23 17:47:04 -0300

    feat/first commit

---
+++ src/main/java/com/grupo1/tpintegrador/TpintegradorApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.tpintegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpintegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpintegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/tpintegrador/TpintegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.tpintegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpintegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
